a5f6b145ea6c0c0d536c8e731211e6c04f04e676
Glitch (github-cloner) <none>
2018-06-07 15:07:35 +0000

    📖🐣 Checkpoint ./start.sh:728660/76 ./index.html:728660/136

--- start.sh
+++ start.sh
@@ -3,7 +3,10 @@ set -e
 
 if [ ! -z "${GITHUB_REPO}" ]; then
   echo "Cloning ${GITHUB_REPO}, please wait..."
-  sleep 5
+  until [ nc -z localhost 1081 ]; do
+    sleep 1
+  done
+  sleep 1
   curl -X POST http://localhost:1083/stop &> /dev/null
   rm -rf /app/* /app/.* /rbd/pnpm-volume/app/node_modules &> /dev/null || true
   git init
